9f84516395f150a78581795b1542617cfa00b971
Jadilson Guedes <jadilson12@gmail.com>
2019-10-05 19:43:39 -0300

    [UPDATE] Atualizando componentes pessoas e lancamentos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { TooltipModule } from 'primeng/tooltip';
 
+
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LancamentoPesquisaComponent } from './lancamento-pesquisa/lancamento-pesquisa.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { PessoasComponent } from './pessoas/pessoas.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LancamentoPesquisaComponent,
+    NavbarComponent,
+    PessoasComponent
   ],
   imports: [
     BrowserModule,
+    FontAwesomeModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+
+    InputTextModule,
+    ButtonModule,
+    TableModule,
+    TooltipModule
 
   ],
   providers: [],
