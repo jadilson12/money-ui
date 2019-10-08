4b580c0ea8739136b3e179e5c9c8ddc66e36c129
Jadilson Guedes <jadilson12@gmail.com>
2019-10-08 09:04:12 -0300

    [ADD] Criando o CoreModule e add o navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-
-import { PessoasModule } from './pessoas/pessoas.module';
-import { LancamentosModule } from './lancamentos/lancamentos.module';
+import { PessoasModule } from "./pessoas/pessoas.module";
+import { LancamentosModule } from "./lancamentos/lancamentos.module";
+import { CoreModule } from "./core/core.module";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavbarComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    FontAwesomeModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+
     PessoasModule,
     LancamentosModule,
-
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
