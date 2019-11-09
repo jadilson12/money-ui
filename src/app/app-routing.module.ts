c9415d8bf8c4721679c627cd088e18af75cd94b5
Jadilson Guedes <jadilson12@gmail.com>
2019-11-09 18:28:22 -0300

    [ADD] Atualizando routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {PessoasPesquisaComponent} from "./pessoas/pessoas-pesquisa/pessoas-pesquisa.component";
+import {LancamentoPesquisaComponent} from "./lancamentos/lancamento-pesquisa/lancamento-pesquisa.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: PessoasPesquisaComponent},
+  {path: "pessoas", component: PessoasPesquisaComponent},
+  {path: "lancamentos", component: LancamentoPesquisaComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
